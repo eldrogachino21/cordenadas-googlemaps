a6eebf9bfdc2fd10211fbe54389ee34220ec8e8e
eldrogachino21 <57367901+eldrogachino21@users.noreply.github.com>
2022-01-12 16:39:49 -0600

    s

--- index.js
+++ index.js
@@ -69,6 +69,7 @@ localStorage.setItem("ubicaciones", JSON.stringify(carrito));
 var map = L.map('map').setView([20.648206, -103.353882], 13);
 
 function init(){
+    map = L.map('map').setView([20.648206, -103.353882], 13);
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
