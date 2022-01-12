61495d3290d3f571d4bd1946a73254f330813062
eldrogachino21 <57367901+eldrogachino21@users.noreply.github.com>
2022-01-12 16:59:26 -0600

    s

--- index.js
+++ index.js
@@ -88,7 +88,7 @@ var array =JSON.parse(localStorage.getItem("ubicaciones"))
             console.log(element)
             console.log(parseFloat(element.latitud),parseFloat(element.longitud))
   marker = L.marker([parseFloat(element.longitud),parseFloat(element.latitud) ]).addTo(map);
-marker.bindPopup(`<b>${element.nombre}</b><br><button onclick="editar(${element.longitud},${element.latitud})">Editar</button>`).openPopup();
+marker.bindPopup(`<b>${element.nombre}</b><br><button onclick="editar(${element.nombre},${element.longitud},${element.latitud})">Editar</button>`).openPopup();
             
         });
 
@@ -141,7 +141,8 @@ function register(){
     alert("ok registrado")
 }
 
-function editar (long,lat){
+function editar (nombre,long,lat){
+    document.getElementById("nombre").value = nombre
     document.getElementById("latitud").value = lat,
     document.getElementById("longitud").value = long
 }
