8288a26ca424bd7e071f1648dfdb8c4af0bffac6
eldrogachino21 <57367901+eldrogachino21@users.noreply.github.com>
2022-01-12 16:06:46 -0600

    s

--- index.js
+++ index.js
@@ -85,7 +85,7 @@ var array =JSON.parse(localStorage.getItem("ubicaciones"))
             console.log(element)
             console.log(parseFloat(element.latitud),parseFloat(element.longitud))
  var marker = L.marker([parseFloat(element.longitud),parseFloat(element.latitud) ]).addTo(map);
-marker.bindPopup("<b>Hola soy un marcador</b><br><button>Editar</button>").openPopup();
+marker.bindPopup(`<b>${element.nombre}</b><br><button>Editar</button>`).openPopup();
             
         });
 
