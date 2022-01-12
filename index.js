66f93b525a284042500616a42db61da490a80074
eldrogachino21 <57367901+eldrogachino21@users.noreply.github.com>
2022-01-12 10:22:25 -0600

    s

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
 var map = L.map('map').setView([51.505, -0.09], 13);
 
-L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    id: 'mapbox/streets-v11',
-    tileSize: 512,
-    zoomOffset: -1,
-    accessToken: 'your.mapbox.access.token'
+L.tileLayer("https://(s).tile.openstreetnap.org/[)/[x)/(y)-prg",{
+    attribution: "&copy: ca href-'https://w.openstreetnap.org/copyright'>0penstreetlapc/a> contributors"
 }).addTo(map);
 var marker = L.marker([51.5, -0.09]).addTo(map);
 
